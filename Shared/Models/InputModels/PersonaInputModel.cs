75452bc9d6e60aaf0c9616f60d4f5621275a0a62
Angelo Pirola <angelo9871@gmail.com>
2022-01-23 18:55:53 +0100

    Refactoring progetto SHARED

---
+++ Shared/Models/InputModels/PersonaInputModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoBlazorApp.Shared.Models.InputModels
+{
+    public class PersonaInputModel
+    {
+        public int PersonaId { get; set; }
+
+        [Required(ErrorMessage = "Il cognome è obbligatorio"),
+        Display(Name = "Cognome")]
+        public string Cognome { get; set; }
+
+        [Required(ErrorMessage = "Il nome è obbligatorio"),
+        Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "L'indirizzo email è obbligatorio"),
+        EmailAddress(ErrorMessage = "Devi inserire un indirizzo email"),
+        Display(Name = "Indirizzo Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Il telefono è obbligatorio"),
+        Display(Name = "Telefono")]
+        public string Telefono { get; set; }
+    }
+}
---
+++ Shared/Models/ViewModels/PersonaViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoBlazorApp.Shared.Models.ViewModels
+{
+    public class PersonaViewModel
+    {
+        public int PersonaId { get; set; }
+        public string Cognome { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+    }
+}
